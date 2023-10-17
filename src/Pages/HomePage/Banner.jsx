55be298b974ce771c100eb0fb63a4e2189721bd3
Ahammad204 <Ahammadcode@gmail.com>
2023-10-17 20:50:35 +0600

    Banner added to the home page

---
+++ src/Pages/HomePage/Banner.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="">
+                <div
+                    className="hero bg-cover "
+                    style={{
+                        backgroundImage:
+                            "url(https://i.ibb.co/hWknz4f/2010-i123-005-car-headlights-AD-composition.jpg)",
+                    }}
+                >
+                    <div className="hero-overlay bg-opacity-70"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div className="">
+                            <p className="mb-5 text-4xl md:text-9xl font-extrabold font-teko text-white">
+                                PH Mot<span className="text-[#ED1D26]">e</span>x
+                            </p>
+                            <div className="">
+                                <p className="text-3xl md:text-6xl font-teko font-semibold text-white">
+                                    Best way to find your <span className="text-[#ED1D26]">Dream</span> car
+                                </p>
+
+                                <p className="mt-6 mb-3 font-bold text-lg text-center">
+                                    Discover the Road to Your Dream Car at Motex: Where Driving Excellence Begins!
+
+                           
+                                </p>
+
+                                <Link
+                                    to="/about"
+                                    className="block md:inline-block mt-4"
+                                >
+                                    <button className="btn bg-[#ED1D26] hover:bg-[#ED1D26] text-white border-none ">
+                                        Discover Now
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,18 @@
+import Banner from "./Banner";
 import Navbar from "./Navbar";
 
 
 const HomePage = () => {
     return (
         <div>
+            <div>
             <Navbar></Navbar>
+            </div>
+            <div>
+                <Banner></Banner>
+            </div>
         </div>
+        
     );
 };
 
