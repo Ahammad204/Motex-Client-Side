187f1caf35ee09415016f6caeca450d2ce482422
Ahammad204 <Ahammadcode@gmail.com>
2023-10-19 22:55:14 +0600

    Add dynamic rating

--- src/Pages/AddProduct/AppProduct.jsx
+++ src/Pages/AddProduct/AppProduct.jsx
@@ -7,11 +7,11 @@ const AppProduct = () => {
         const form = event.target;
 
         const name = form.name.value;
-        const brand = form.brand.value.toUpperCase();
+        const brand = form.brand.value;
         const type = form.type.value;
         const price = form.price.value;
         const short = form.short.value;
-        const rating2 = form.rating2.value;
+        const rating2 = parseInt(form.rating2.value, 10);
         const photo = form.photo.value;
 
         const newCar = { name,brand,type,price,short,rating2,photo }
@@ -68,13 +68,19 @@ const AppProduct = () => {
                         </label>
                     </div>
                     <div className="form-control md:w-1/2 ml-4">
-                        <label className="label">
-                            <span className="label-text">Brand Name</span>
+                
+                         <label className="label">
+                            <span className="label-text">Car Type</span>
                         </label>
-                        <label className="input-group">
-
-                            <input type="text" name="brand" placeholder="Brand Name" className="input input-bordered w-full" />
-                        </label>
+                        <select name="brand" className="select w-full outline-2 outline-black">
+                            <option disabled selected>Choose your car type</option>
+                            <option>TOYOTA</option>
+                            <option>FORD</option>
+                            <option>BMW</option>
+                            <option>MARCEDES_BENZ</option>
+                            <option>TESLA</option>
+                            <option>HONDA</option>
+                        </select>
                     </div>
                 </div>
                 {/* Supplier and test */}
@@ -86,7 +92,7 @@ const AppProduct = () => {
                         <select name="type" className="select w-full ">
                             <option disabled selected>Choose your car type</option>
                             <option>Sports</option>
-                            <option>SUB</option>
+                            <option>SUV</option>
                             <option>Electric</option>
                             <option>Off Road</option>
                             <option>Classic</option>
@@ -120,11 +126,11 @@ const AppProduct = () => {
                             <span className="label-text">Rating</span>
                         </label>
                         <div  className="rating" >
-                            <input type="radio" name="rating2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating2" className="mask mask-star-2 bg-orange-400"  />
-                            <input type="radio" name="rating2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating2" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating2" value="1" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating2" value="2" className="mask mask-star-2 bg-orange-400"  />
+                            <input type="radio" name="rating2" value="3" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating2" value="4" className="mask mask-star-2 bg-orange-400" />
+                            <input type="radio" name="rating2" value="5" className="mask mask-star-2 bg-orange-400" />
                         </div>
                     </div>
                 </div>
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
+import Rating from "react-rating";
 import { Link, useParams } from "react-router-dom";
 
 
@@ -46,44 +47,45 @@ const Products = () => {
 
     return (
         <div>
+
+            <div>
+
+                <div className="">
+                    {slider?.map(slide => (
+                        <div className="carousel w-full h-96" key={slide._id} >
+                            <div id="slide1" className="carousel-item relative w-full h-96">
+                                <img src={slide.photo} className="w-full" />
+                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                    <a href="#slide3" className="btn btn-circle">❮</a>
+                                    <a href="#slide2" className="btn btn-circle">❯</a>
+                                </div>
+                            </div>
+                            <div id="slide2" className="carousel-item relative w-full h-96">
+                                <img src={slide.photo2} className="w-full" />
+                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                    <a href="#slide1" className="btn btn-circle">❮</a>
+                                    <a href="#slide3" className="btn btn-circle">❯</a>
+                                </div>
+                            </div>
+                            <div id="slide3" className="carousel-item relative w-full">
+                                <img src={slide.photo3} className="w-full" />
+                                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                                    <a href="#slide2" className="btn btn-circle">❮</a>
+                                    <a href="#slide1" className="btn btn-circle">❯</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+            <h1 className="text-center font-bold text-6xl mb-5 mt-5"> Our Top Quality <span className="text-[#ED1D26]">  Cars</span></h1>
+            <hr />
             {products.length > 0 ? (
 
                 <div>
 
-                    <div>
-
-                        <div className="">
-                            {slider?.map(slide => (
-                                <div className="carousel w-full h-96" key={slide._id} >
-                                    <div id="slide1" className="carousel-item relative w-full h-96">
-                                        <img src={slide.photo} className="w-full" />
-                                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                            <a href="#slide3" className="btn btn-circle">❮</a>
-                                            <a href="#slide2" className="btn btn-circle">❯</a>
-                                        </div>
-                                    </div>
-                                    <div id="slide2" className="carousel-item relative w-full h-96">
-                                        <img src={slide.photo2} className="w-full" />
-                                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                            <a href="#slide1" className="btn btn-circle">❮</a>
-                                            <a href="#slide3" className="btn btn-circle">❯</a>
-                                        </div>
-                                    </div>
-                                    <div id="slide3" className="carousel-item relative w-full">
-                                        <img src={slide.photo3} className="w-full" />
-                                        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                                            <a href="#slide2" className="btn btn-circle">❮</a>
-                                            <a href="#slide1" className="btn btn-circle">❯</a>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            ))}
-                        </div>
-
-                    </div>
-                    <h1 className="text-center font-bold text-6xl mb-5 mt-5"> Our Top Quality <span className="text-[#ED1D26]">  Cars</span></h1>
-                    <hr />
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
                         {products?.map(prod => (
                             <div className="card bg-base-100 shadow-xl" key={prod._id}>
@@ -94,6 +96,44 @@ const Products = () => {
                                     <p className="text-base font-semibold mt-4">BRAND: {prod.brand}</p>
                                     <p className="text-base font-semibold ">Car Type: {prod.type}</p>
                                     <p className="text-xl font-semibold text-[#ED1D26]">Price: {prod.price}</p>
+                                    <div className="rating">
+
+                                        <Rating
+                                            emptySymbol={
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth={1.5}
+                                                    stroke="currentColor"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                                                    />
+                                                </svg>
+                                            }
+                                            fullSymbol={
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                    fill="currentColor"
+                                                    className="w-6 h-6"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            }
+                                            initialRating={prod.rating2}
+                                            readonly
+                                        />
+                                    </div>
+
                                     <div className="card-actions justify-end">
                                         <button className="btn bg-[#ED1D26] hover:bg-[#ED1D26] text-white">Details</button>
                                         <button className="btn bg-[#ED1D26] hover:bg-[#ED1D26] text-white">Update</button>
@@ -108,33 +148,33 @@ const Products = () => {
             ) : (
 
                 <div>
-                {isLoading ? (
-                    <div className="flex justify-center items-center mt-20">
-                        <div className="hero w-2/3 h-2/3">
-                            <div className=""></div>
-                            <div className="hero-content text-center">
-                                <div className="max-w-md">
-                                <span className="loading loading-bars loading-lg"></span>
+                    {isLoading ? (
+                        <div className="flex justify-center items-center mt-20">
+                            <div className="hero w-2/3 h-2/3">
+                                <div className=""></div>
+                                <div className="hero-content text-center">
+                                    <div className="max-w-md">
+                                        <span className="loading loading-bars loading-lg"></span>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                ) : (
-              
-                    <div className="flex justify-center items-center mt-20">
-                    <div className="hero w-2/3 h-2/3" >
-                        <div className=""></div>
-                        <div className="hero-content text-center ">
-                            <div className="max-w-md">
-                                <h1 className="mb-5 text-5xl font-bold">Hey Car Hunter</h1>
-                                <p className="mb-5 text-xl">There is noting to show <br /> Can you want to add Something</p>
-                                <Link to={"/addProduct"}><button className="btn text-white bg-[#ED1D26] hover:bg-[#ED1D26]">Add Car</button></Link>
+                    ) : (
+
+                        <div className="flex justify-center items-center mt-20">
+                            <div className="hero w-2/3 h-2/3" >
+                                <div className=""></div>
+                                <div className="hero-content text-center ">
+                                    <div className="max-w-md">
+                                        <h1 className="mb-5 text-5xl font-bold">Hey Car Hunter</h1>
+                                        <p className="mb-5 text-xl ">There is noting to show <br /> Do you Want to add a car</p>
+                                        <Link to={"/addProduct"}><button className="btn text-white bg-[#ED1D26] hover:bg-[#ED1D26]">Add Car</button></Link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )}
                 </div>
-                )}
-            </div>
 
             )
 
